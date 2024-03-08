0b8ae36209c6b7a731257f657cff79102f012336
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-08 19:06:58 +0330

    create SpinnerIcon

---
+++ src/Components/Modules/SpinnerIcon/SpinnerIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IoIosFlower } from 'react-icons/io'
+
+interface ISpinnerIcon {
+    color: string
+}
+
+export default function SpinnerIcon({color}: ISpinnerIcon) {
+    return (
+        <>
+            <IoIosFlower className={` ${color} text-9xl animate-spin-slow`} />
+        </>
+    )
+}
