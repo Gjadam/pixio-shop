43403c3b975aeee1b09dac3bba684fd5eefdf3a0
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-08 19:06:33 +0330

    changing text color

--- src/Components/Modules/NavBarLink/NavBarLink.tsx
+++ src/Components/Modules/NavBarLink/NavBarLink.tsx
@@ -7,7 +7,7 @@ export default function NavBarLink({text}: TNavBarLink) {
     return (
         <>
             <a href="">
-                <span className=' flex justify-center items-center tracking-wider hover:text-rose-600 font-semibold transition-colors delay-75 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
+                <span className=' flex justify-center items-center tracking-wider hover:text-purple-600 font-semibold transition-colors delay-75 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
             </a>
         </>
     )
