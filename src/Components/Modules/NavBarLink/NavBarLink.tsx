2e86c13f3007b62033f90f81503fca2463bb4646
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-11 17:31:23 +0330

    changing some style

--- src/Components/Modules/NavBarLink/NavBarLink.tsx
+++ src/Components/Modules/NavBarLink/NavBarLink.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-type TNavBarLink = {
+
+interface INavBarLink {
     text: string
 }
 
-export default function NavBarLink({text}: TNavBarLink) {
+export default function NavBarLink({text}: INavBarLink) {
     return (
         <>
             <a href="">
-                <span className=' flex justify-center items-center tracking-wider hover:text-purple-600 font-semibold transition-colors delay-75 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
+                <span className=' flex justify-center items-center tracking-wider hover:text-purple-600 font-semibold transition-colors duration-300 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
             </a>
         </>
     )
