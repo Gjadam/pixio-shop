b5495e224f4bcf1ed46dc9b9dde24986695cc562
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-12 16:18:01 +0330

    creating Button

---
+++ src/Components/Modules/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface IButton {
+    text: string
+    padding: string
+}
+
+export default function Button({text, padding}: IButton) {
+    return (
+        <div className={` overflow-hidden uppercase font-bold  rounded-xl border-1 bg-white border-black text-black hover:bg-black hover:text-white  ${padding} cursor-pointer relative after:absolute after:left-0 after:bottom-0 after:bg-[rgba(255,255,255,0.1)] after:border-0  after:hover:border-r-1 after:border-white  z-0 after:-z-10 after:w-0 after:h-full after:hover:w-[101%]  after:transition-all after:ease-in-out after:duration-500 transition-all duration-300`}>
+            <span>{text}</span>
+        </div>
+    )
+}
