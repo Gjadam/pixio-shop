6c95fc6a9ac6a228062425f37b3d1d2e95c97143
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-17 14:12:14 +0330

    creating SectionHeader

---
+++ src/Components/Modules/SectionHeader/SectionHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface ISectionHeader {
+    text: string
+}
+
+export default function SectionHeader({text}: ISectionHeader) {
+    return (
+        <>
+            <h1 className=' text-4xl font-bold'>{text}</h1>
+        </>
+    )
+}
