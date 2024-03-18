e6f6e18ff0d9dde5d57debdafd097567a35a1a82
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-18 18:19:26 +0330

    creating SponsoredBox

---
+++ src/Components/Modules/SponsoredBox/SponsoredBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SponsoredBox() {
+    return (
+        <>
+            <div className=" m-5 relative overflow-hidden w-80 rounded-2xl group">
+                <img src="/images/jpg/bg-sponsored1.jpg" alt="bg-sponsored" className=' w-full  brightness-75 group-hover:scale-125 transition-transform duration-700' />
+                <div className=" absolute left-[6.3rem] bottom-[4.8rem]">
+                    <img src="/images/png/sponsored1.png" alt="sponsored" className=' w-32' />
+                </div>
+            </div>
+        </>
+    )
+}
