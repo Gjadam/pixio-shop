39957ff3300b3b8a9002430d824072901fe3ce9c
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-18 18:19:45 +0330

    changing SwiperButton colors

--- src/Components/Modules/SwiperButton/SwiperButton.tsx
+++ src/Components/Modules/SwiperButton/SwiperButton.tsx
@@ -6,8 +6,8 @@ export default function SwiperButton() {
   const swiper = useSwiper()
 
   return (
-    <div className=" bg-black p-5 rounded-full absolute -right-10 bottom-[5.6rem] z-10 cursor-pointer" onClick={() => swiper.slideNext()}>
-      <TiArrowRight className='customers__next-btn text-3xl text-white' />
+    <div className=" bg-black text-white hover:bg-white hover:text-black p-5 rounded-full absolute -right-10 bottom-[5.6rem] z-10 cursor-pointer transition-colors duration-300" onClick={() => swiper.slideNext()}>
+      <TiArrowRight className='customers__next-btn text-3xl ' />
     </div>
   )
 }
