20fe01b0803f2b9b8c215bef02dee6840d115425
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-20 18:50:16 +0330

    changing ProductBox size in large size

--- src/Components/Modules/ProductBox/ProductBox.tsx
+++ src/Components/Modules/ProductBox/ProductBox.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaRegHeart } from 'react-icons/fa'
 import { PiBasket } from "react-icons/pi";
-
+import { FaEye } from "react-icons/fa";
 interface IProductBox {
     size?: string
 }
@@ -11,8 +11,8 @@ export default function ProductBox({size}: IProductBox) {
         <div className={` ${size === 'lg' ? 'w-72' : 'w-48'} flex justify-center items-center flex-col`}>
             <div className={` group relative  w-full rounded-3xl overflow-hidden cursor-pointer`}>
                 <img src="/images/png/product1.png" alt="product" className=' rounded-3xl group-hover:-translate-y-6 transition-transform duration-1000 ease-in-out' />
-                <div className={`  px-6 py-3 text-center border-4 border-white bg-black hover:bg-purple-600 rounded-full overflow-hidden absolute ${size === 'lg' ? 'left-[5.5rem]' : 'left-[1.7rem]'}  -bottom-10 opacity-0 group-hover:opacity-100 group-hover:bottom-0 transition-all duration-500 ease-in-out  after:absolute after:left-0  after:bottom-0 after:bg-[rgba(255,255,255,0.1)] after:border-0  after:hover:border-r-1 after:border-white  z-0 after:-z-10 after:w-0 after:h-full after:hover:w-[101%]  after:transition-all after:ease-in-out after:duration-500 `}>
-                    <span className=' text-sm text-white uppercase'>Quick View</span>
+                <div className={`  px-7 py-3 text-center border-4 border-primary bg-black hover:bg-purple-600 rounded-full overflow-hidden absolute ${size === 'lg' ? 'left-[4.5rem]' : 'left-[3.5rem]'}  -bottom-10 opacity-0 group-hover:opacity-100 group-hover:bottom-0 transition-all duration-500 ease-in-out  after:absolute after:left-0  after:bottom-0 after:bg-[rgba(255,255,255,0.1)] after:border-0  after:hover:border-r-1 after:border-white  z-0 after:-z-10 after:w-0 after:h-full after:hover:w-[101%]  after:transition-all after:ease-in-out after:duration-500 `}>
+                    <span className=' text-sm text-white uppercase'>{size === 'lg' ? 'Quick View' : <FaEye className='text-xl' />}</span>
                 </div>
 
                 <div className=" absolute top-6 left-5">
