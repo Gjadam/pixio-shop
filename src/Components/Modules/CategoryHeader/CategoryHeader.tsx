72eba7079477f1847bc9455dbd1a8cf13789c3be
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-21 13:58:08 +0330

    using BreadCrumb in categoryHeader

--- src/Components/Modules/CategoryHeader/CategoryHeader.tsx
+++ src/Components/Modules/CategoryHeader/CategoryHeader.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import { IoIosArrowForward } from 'react-icons/io'
+import BreadCrumb from '../BreadCrumb/BreadCrumb'
 
 export default function CategoryHeader() {
     return (
         <div className=" flex justify-center items-center w-full h-64 relative after:absolute after:bg-[url('/images/jpg/categoryHeader-bg.jpg')] after:w-full after:h-full after:z-0  after:bg-no-repeat after:bg-cover after:bg-center text-white after:brightness-50 ">
             <div className="flex justify-center items-center flex-col gap-4 z-50">
                 <h1 className=' text-5xl font-bold z-50'>Shop</h1>
-                <div className=" flex justify-center items-center gap-3">
-                    <span className=' flex justify-center items-center'>Home</span>
-                    <IoIosArrowForward />
-                    <span className=' flex justify-center items-center'>test</span>
-                </div>
+                <BreadCrumb />
             </div>
         </div>
     )
