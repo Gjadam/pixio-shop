7ecf93d2d9380478ae841a0e6220326c0610e379
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-22 12:29:39 +0330

    update image url

--- src/Components/Modules/NavBarLink/NavBarLink.tsx
+++ src/Components/Modules/NavBarLink/NavBarLink.tsx
@@ -8,7 +8,7 @@ export default function NavBarLink({text}: INavBarLink) {
     return (
         <>
             <a href="">
-                <span className=' flex justify-center items-center tracking-wider hover:text-purple-600 font-semibold transition-colors duration-300 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
+                <span className=' flex justify-center items-center tracking-wider hover:text-purple-600 font-semibold transition-colors duration-300 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("/images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
             </a>
         </>
     )
