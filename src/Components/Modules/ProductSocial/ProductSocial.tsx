19e59274d41454138154ef6f5f7fe27a6cb8f2ab
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-22 12:30:22 +0330

    creating ProductSocial component

---
+++ src/Components/Modules/ProductSocial/ProductSocial.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IProductSocial {
+    text: string
+}
+
+export default function ProductSocial({text}: IProductSocial) {
+    return (
+        <a href="">
+            <span className='uppercase font-bold text-md hover:text-purple-600 transition-colors duration-200 inline-block'>{text}</span>
+        </a>
+    )
+}
