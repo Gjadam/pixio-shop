1d482fa703603c7162967657801bb5d18ef5e2f8
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-27 18:54:18 +0330

    changing position of image

--- src/Components/Modules/SponsoredBox/SponsoredBox.tsx
+++ src/Components/Modules/SponsoredBox/SponsoredBox.tsx
@@ -5,7 +5,7 @@ export default function SponsoredBox() {
         <>
             <div className=" m-5 relative overflow-hidden w-80 rounded-2xl group">
                 <img src="/images/jpg/bg-sponsored1.jpg" alt="bg-sponsored" className=' w-full  brightness-75 group-hover:scale-125 transition-transform duration-700' />
-                <div className=" absolute left-[6.3rem] bottom-[4.8rem]">
+                <div className=" absolute left-[5rem] bottom-[4rem]">
                     <img src="/images/png/sponsored1.png" alt="sponsored" className=' w-32' />
                 </div>
             </div>
