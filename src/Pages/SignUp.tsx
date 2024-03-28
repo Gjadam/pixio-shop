661a14f30ef393d65adbc4f83bc5f757fca2d227
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-28 16:41:30 +0330

    creating SignUp page

---
+++ src/Pages/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Components/Templates/NavBar/NavBar'
+import Registration from '../Components/Templates/Registration/Registration'
+import Footer from '../Components/Templates/Footer/Footer'
+
+export default function SignUp() {
+    return (
+        <>
+            <NavBar />
+            <Registration type={'SignUp'} />
+            <Footer />
+        </>
+    )
+}
