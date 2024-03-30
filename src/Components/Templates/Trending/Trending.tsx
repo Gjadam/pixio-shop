030bfbeffeb0977b1e1db25f0fa40d1ff697fa0f
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-30 15:44:04 +0330

    responsive

--- src/Components/Templates/Trending/Trending.tsx
+++ src/Components/Templates/Trending/Trending.tsx
@@ -15,17 +15,31 @@ export default function Trending() {
     return (
         <div className=' mt-28'>
             <div className=" container mx-auto flex justify-between items-center">
-                <div className=" max-w-[35rem]">
+                <div className=" max-w-[35rem] md:text-start text-center p-5">
                 <SectionHeader text='Discover The Most Trending Post In Pixio.' />
                 </div>
+                <div className=" hidden md:block">
                 <SpinnerIcon color='text-purple-500' />
+                </div>
             </div>
-            <div className="mt-8">
+            <div className="  mt-8">
                 <Swiper
-                    slidesPerView={5}
+                 breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                    },
+                    720: {
+                        slidesPerView: 2,
+                    },
+                    1200: {
+                        slidesPerView: 3,
+                    },
+                    1400: {
+                        slidesPerView: 4,
+                    },
+                }}
                     centeredSlides={true}
                     speed={1000}
-                    spaceBetween={250}
                     grabCursor={true}
                     className="mySwiper"
                 >
