e56d7d0773c7be4c43019b946315ee517d1cb381
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-30 19:43:51 +0330

    adding Swiper.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './Styles/Fonts.css'
+import './Styles/Swiper.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
