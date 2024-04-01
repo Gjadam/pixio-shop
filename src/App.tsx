1018f2a7f2a35320ed5ece4165deed0ac0fe43f8
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-02 01:13:38 +0330

    removing useless imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
