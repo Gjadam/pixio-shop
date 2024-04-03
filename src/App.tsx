298c6eacd844fd306b2688d2f93c65314e7056a7
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-03 13:08:05 +0330

    adding navbar and footer in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
+import NavBar from './Components/Templates/NavBar/NavBar'
+import Footer from './Components/Templates/Footer/Footer'
 
 function App() {
   const router = useRoutes(routes)
   return (
     <>
-    {router}
+      <NavBar />
+      {router}
+      <Footer />
     </>
   )
 }
