4f8d10f20547a94a3f1a2fa2c01d45f8526c1219
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-05 21:27:35 +0330

    removing navbar and footer in sign-up page

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,13 +1,9 @@
-import NavBar from '../Components/Templates/NavBar/NavBar'
 import Registration from '../Components/Templates/Registration/Registration'
-import Footer from '../Components/Templates/Footer/Footer'
 
 export default function SignUp() {
     return (
         <>
-            <NavBar />
             <Registration type={'SignUp'} />
-            <Footer />
         </>
     )
 }
