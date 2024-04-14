d5d6cf2a0b2072014d5b3e97f924a4f561e91792
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-14 14:25:47 +0330

    changing  some styles

--- src/Components/Modules/TopUp/TopUp.tsx
+++ src/Components/Modules/TopUp/TopUp.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { TiArrowUp } from "react-icons/ti";
+import { IoIosArrowUp } from "react-icons/io";
+import { MdBlock } from "react-icons/md";
 
 export default function TopUp() {
 
@@ -25,10 +26,16 @@ export default function TopUp() {
 
     return (
         <div className='flex justify-center items-center fixed -right-16 md:-right-5 bottom-1/2 z-50 rotate-90 group cursor-pointer' onClick={scrollToUpHandler}>
-            <TiArrowUp className=" text-xl -rotate-90 mt-0.5 translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300" />
+            {
+                scrollWidth === 0 ? (
+                    <MdBlock className="   -rotate-90 mt-0.5 translate-x-5 group-hover:-translate-x-1 opacity-0 group-hover:opacity-100 transition-all duration-300" />
+                ) : (
+                    <IoIosArrowUp className="  -rotate-90 mt-0.5 translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300" />
+                )
+            }
             <span className=" font-bold text-xs mr-4 transition-all duration-300">Scroll to up</span>
             <div className="bg-zinc-300 h-[1px]  w-24 rounded-full   ">
-                <div style={{ width: `${scrollWidth}%` }} className="h-full  relative after:absolute bg-black after:bg-black after:right-0 after:-top-[4px] after:p-[0.30rem] after:rounded-full"></div>
+                <div style={{ width: `${scrollWidth}%` }} className="h-full  relative after:absolute bg-black after:bg-black after:right-0 after:-top-[5px] after:p-[0.30rem] after:border-1 after:border-primary after:rounded-full"></div>
             </div>
         </div>
     )
