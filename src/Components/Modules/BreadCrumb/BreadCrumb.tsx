090a976687d09bc39948ba76378142a602d52825
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-19 14:00:17 +0330

    BreadCrumb update

--- src/Components/Modules/BreadCrumb/BreadCrumb.tsx
+++ src/Components/Modules/BreadCrumb/BreadCrumb.tsx
@@ -1,11 +1,19 @@
 import { IoIosArrowForward } from 'react-icons/io'
+import { GoHomeFill } from "react-icons/go";
+import { Link } from 'react-router-dom'
 
-export default function BreadCrumb() {
+interface IBreadCrumb {
+    path: string
+}
+
+export default function BreadCrumb({ path }: IBreadCrumb) {
     return (
-        <div className=" flex justify-center items-center gap-3">
-            <span className=' flex justify-center items-center'>Home</span>
+        <div className=" flex justify-center items-center gap-3 font-bold">
+            <Link to={'/'}>
+                <span className=' flex justify-center items-center gap-1 hover:text-purple-600 transition-colors'><GoHomeFill className='mb-0.5'/>Home</span>
+            </Link>
             <IoIosArrowForward />
-            <span className=' flex justify-center items-center'>Product Carousel</span>
+            <span className=' flex justify-center items-center capitalize'>{path}</span>
         </div>
     )
 }
