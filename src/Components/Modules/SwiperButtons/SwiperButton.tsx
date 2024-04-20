8657b8e6ed04e9293c0eab278b7bf993317a341c
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-20 18:49:22 +0330

    SwiperButton update

---
+++ src/Components/Modules/SwiperButtons/SwiperButton.tsx
@@ -0,0 +1,12 @@
+import { TiArrowRight } from 'react-icons/ti'
+import { useSwiper } from 'swiper/react'
+
+export default function SwiperButton() {
+  const swiper = useSwiper()
+
+  return (
+    <div className=" bg-black text-white hover:bg-primary hover:text-black p-5 rounded-full absolute -right-10 bottom-[5.6rem] z-10 cursor-pointer transition-colors duration-300" onClick={() => swiper.slideNext()}>
+      <TiArrowRight className='customers__next-btn text-3xl ' />
+    </div>
+  )
+}
