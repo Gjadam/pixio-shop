9cbc9c566004aad3c7127901a8564c994fda91fe
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-25 14:05:42 +0330

    using path in BreadCrumb

--- src/Components/Modules/CategoryHeader/CategoryHeader.tsx
+++ src/Components/Modules/CategoryHeader/CategoryHeader.tsx
@@ -5,7 +5,7 @@ export default function CategoryHeader() {
         <div className=" flex justify-center items-center w-full h-64 relative after:absolute after:bg-[url('/images/jpg/categoryHeader-bg.jpg')] after:w-full after:h-full after:z-0  after:bg-no-repeat after:bg-cover after:bg-center text-white after:brightness-50 ">
             <div className="flex justify-center items-center flex-col gap-4 z-50">
                 <h1 className=' text-5xl font-bold z-50'>Shop</h1>
-                <BreadCrumb />
+                <BreadCrumb path='Shop' />
             </div>
         </div>
     )
