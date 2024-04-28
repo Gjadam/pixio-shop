decde2a16013ff250d876b83001ba03d3b5baef3
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-28 12:03:27 +0330

    update NavBarLink

--- src/Components/Modules/NavBarLink/NavBarLink.tsx
+++ src/Components/Modules/NavBarLink/NavBarLink.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom"
 
 interface INavBarLink {
     text: string
+    path: string
 }
 
-export default function NavBarLink({text}: INavBarLink) {
+export default function NavBarLink({text, path}: INavBarLink) {
     return (
         <>
-            <a href="">
+            <Link to={path}>
                 <span className=' flex justify-center items-center tracking-wider hover:text-purple-600 font-semibold transition-colors duration-300 ease-in-out relative after:absolute after:-right-4 after:w-3 after:h-3 after:bg-[url("/images/svg/star.svg")] after:bg-no-repeat'>{text}</span>
-            </a>
+            </Link>
         </>
     )
 }
