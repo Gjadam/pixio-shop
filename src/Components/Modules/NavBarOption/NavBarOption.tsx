7655ea1748110824c8edcfb6424e357832a43c6d
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-04-30 00:09:33 +0330

    update

--- src/Components/Modules/NavBarOption/NavBarOption.tsx
+++ src/Components/Modules/NavBarOption/NavBarOption.tsx
@@ -8,7 +8,7 @@ interface INavBarOption {
 
 export default function NavBarOption({title, icon, onClickHandler}: INavBarOption) {
     return (
-        <div className={`flex justify-start items-center gap-2 border-b-1 px-6 py-2 ${title === 'Logout' && 'hover:bg-rose-600 border-b-0' } hover:bg-purple-600 hover:text-white  transition-colors duration-300 cursor-pointer group/setting`} onClick={onClickHandler}>
+        <div className={`flex justify-start items-center gap-2 border-b-1 px-6 py-2 ${title === 'Logout' && 'hover:bg-rose-600' } hover:bg-purple-600 hover:text-white  transition-colors duration-300 cursor-pointer group/setting`} onClick={onClickHandler}>
             {icon}
             <div className="flex justify-center items-start flex-col">
                 <span className=' text-sm font-bold'>{title}</span>
--- src/Components/Modules/PopularFilterItem/PopularFilterItem.tsx
+++ src/Components/Modules/PopularFilterItem/PopularFilterItem.tsx
@@ -9,9 +9,9 @@ export default function PopularFilterItem({ text, isSelected }: IPopularFilterIt
         <>
             {
                 isSelected ? (
-                    <span className='w-16 text-sm text-center rounded-full text-white bg-black  p-1  cursor-pointer capitalize'>{text}</span>
+                    <span className='w-16 text-xs lg:text-sm text-center rounded-full text-white bg-black  p-1  cursor-pointer capitalize'>{text}</span>
                 ) : (
-                    <span className='w-16 text-sm text-center p-1 mx-1 cursor-pointer capitalize'>{text}</span>
+                    <span className='w-16 text-xs lg:text-sm text-center p-1 mx-1 cursor-pointer capitalize'>{text}</span>
                 )
             }
         </>
