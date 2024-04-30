e8b35846c6deec4d94e260d63630ec1b2001b855
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-05-01 01:03:22 +0330

    update

--- src/Components/Modules/TopUp/TopUp.tsx
+++ src/Components/Modules/TopUp/TopUp.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { IoIosArrowUp } from "react-icons/io";
-import { MdBlock } from "react-icons/md";
+import { CgScrollH } from "react-icons/cg";
 
 export default function TopUp() {
 
@@ -26,14 +25,8 @@ export default function TopUp() {
 
     return (
         <div className='flex justify-center items-center fixed -right-16 md:-right-5 bottom-1/2 z-50 rotate-90 group cursor-pointer' onClick={scrollToUpHandler}>
-            {
-                scrollWidth === 0 ? (
-                    <MdBlock className="-rotate-90 mt-0.5 translate-x-5 group-hover:-translate-x-1 opacity-0 group-hover:opacity-100 transition-all duration-300" />
-                ) : (
-                    <IoIosArrowUp className="-rotate-90 mt-0.5 translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300" />
-                )
-            }
-            <span className=" font-bold text-xs mr-4 transition-all duration-300">Scroll to up</span>
+            <span className={` ${scrollWidth === 0 && ' invisible'} text-xs mb-1 mr-2 translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 transition-all duration-300`} >Scroll to up</span>
+            <span className=" font-bold text-xs mr-4 transition-all duration-300"><CgScrollH className=" text-xl" /></span>
             <div className="bg-zinc-300 h-[1px]  w-24 rounded-full   ">
                 <div style={{ width: `${scrollWidth}%` }} className="h-full  relative after:absolute bg-black after:bg-black after:right-0 after:-top-[5px] after:p-[0.30rem] after:border-1 after:border-primary after:rounded-full"></div>
             </div>
