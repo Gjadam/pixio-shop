28d1baeb66d0616004ef14ca1671ff33f0c1b619
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-05-03 14:49:39 +0330

    update SponsoredBox

--- src/Components/Modules/SponsoredBox/SponsoredBox.tsx
+++ src/Components/Modules/SponsoredBox/SponsoredBox.tsx
@@ -1,13 +1,24 @@
 
-export default function SponsoredBox() {
+
+interface ISponsoredBox {
+    bgUrl: string;
+    logoUrl: string;
+    title: string;
+}
+
+export default function SponsoredBox({bgUrl, logoUrl, title}: ISponsoredBox) {
     return (
-        <>
-            <div className=" m-5 relative overflow-hidden w-80 rounded-2xl group">
-                <img src="/images/jpg/bg-sponsored1.jpg" alt="bg-sponsored" className=' w-full  brightness-75 group-hover:scale-125 transition-transform duration-700' />
+        <div className=" flex flex-col">
+            <div className=" relative overflow-hidden w-80 rounded-3xl group">
+                <img src={bgUrl} alt="bg-sponsored" className=' w-full  brightness-50 group-hover:scale-125 transition-transform duration-700' />
                 <div className=" absolute left-[5rem] bottom-[4rem]">
-                    <img src="/images/png/sponsored1.png" alt="sponsored" className=' w-32' />
+                    <img src={logoUrl} alt="sponsored" className=' w-32' />
                 </div>
             </div>
-        </>
+            <div className=" flex justify-between items-center">
+            <span className=" font-bold text-lg p-2">{title}</span>
+            <span className=" text-sm">Min. 30% Off</span>
+            </div>
+        </div>
     )
 }
