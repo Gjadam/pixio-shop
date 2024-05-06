2960bad56432949c5e821b9f75f4cfa9e53919c1
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-05-06 14:04:06 +0330

    update Popular

--- src/Components/Templates/Popular/Popular.tsx
+++ src/Components/Templates/Popular/Popular.tsx
@@ -1,7 +1,36 @@
 import SectionHeader from '../../Modules/SectionHeader/SectionHeader'
 import PopularFilterItem from '../../Modules/PopularFilterItem/PopularFilterItem'
 import ProductBox from '../../Modules/ProductBox/ProductBox';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProductsFromServer } from '../../../Redux/store/products';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import type { RootState } from '@reduxjs/toolkit/query';
+
+export type IProducts = [
+  {
+    collection: number
+    id: number
+    images: [] | undefined
+    inventory: number
+    price_with_tax: number
+    slug: string
+    title: string
+    unit_price: number
+  }
+]
+
 export default function Popular() {
+
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
+  const allProducts = useSelector((state: RootState) => state.products.allProductsData)
+  
+  useEffect(() => {
+    dispatch(getAllProductsFromServer())
+  }, [])
+  
+  
+
   return (
     <div className=' container mx-auto my-24 '>
       <div className=" flex justify-between md:flex-row flex-col gap-3 items-center">
@@ -14,11 +43,12 @@ export default function Popular() {
           <PopularFilterItem text='Jacket' />
         </div>
       </div>
-      <div className=" flex justify-center items-center flex-wrap gap-8 p-12 mx-auto   ">
-        <ProductBox size='lg' />
-        <ProductBox size='lg' />
-        <ProductBox size='lg' />
-        <ProductBox size='lg' />
+      <div className=" flex justify-center items-start flex-wrap gap-8 p-12 mx-auto   ">
+        {
+          allProducts?.slice(0, 8).map(product => (
+            <ProductBox size='lg' key={product.id} {...product} />
+          ))
+        }
       </div>
     </div>
   )
