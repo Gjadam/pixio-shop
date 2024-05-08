b2e0a6987255985c44f1a998724cdc6f05e772d7
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-05-09 03:10:50 +0330

    update

--- src/Components/Modules/NavBarLinkMobile/NavBarLinkMobile.tsx
+++ src/Components/Modules/NavBarLinkMobile/NavBarLinkMobile.tsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom"
 
 interface INavBarLinkMobile {
     text: string
+    path: string
 }
 
-export default function NavBarLinkMobile({ text }: INavBarLinkMobile) {
+export default function NavBarLinkMobile({ text, path }: INavBarLinkMobile) {
     return (
-        <div className="border-b-1 p-3 hover:bg-zinc-900 hover:text-primary transition-colors duration-300 cursor-pointer overflow-hidden  ">
-            <span className=' font-bold '>{text}</span>
-        </div>
+        <Link to={path}>
+            <div className="border-b-1 p-3 hover:bg-zinc-900 hover:text-primary transition-colors duration-300 cursor-pointer overflow-hidden  ">
+                <span className=' font-bold '>{text}</span>
+            </div>
+        </Link>
     )
 }
