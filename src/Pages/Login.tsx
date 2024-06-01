11af0b8d334ebff272a01cbe804903e32e803cb6
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-06-01 14:57:34 +0330

    update

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,5 +1,3 @@
-import Footer from '../Components/Templates/Footer/Footer'
-import NavBar from '../Components/Templates/NavBar/NavBar'
 import Registration from '../Components/Templates/Registration/Registration'
 
 export default function Login() {
