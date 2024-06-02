d07d6793ce3194829e21e83c5b7b4a72b295e5fc
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-06-02 22:00:05 +0330

    update

--- src/Components/Modules/HeaderProductBox/HeaderProductBox.tsx
+++ src/Components/Modules/HeaderProductBox/HeaderProductBox.tsx
@@ -1,25 +1,11 @@
-import React, { useEffect } from 'react'
 import Button from '../Button/Button'
 import SpinnerIcon from '../SpinnerIcon/SpinnerIcon'
 import SwiperButton from '../SwiperButtons/SwiperButton'
 import { IProductBox } from '../ProductBox/ProductBox'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '@reduxjs/toolkit/query'
-import { ThunkDispatch } from '@reduxjs/toolkit'
-import { getSingleCollectionFromServer } from '../../../Redux/store/collections'
 import { Link } from 'react-router-dom'
 
-export default function HeaderProductBox({ id, title, collection, images, unit_price }: IProductBox) {
+export default function HeaderProductBox({ id, title, images, unit_price }: IProductBox) {
 
-
-    const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
-    const singleCollection = useSelector((state: RootState) => state.collections.singleCollection)
-
-
-
-    useEffect(() => {
-        dispatch(getSingleCollectionFromServer(collection))
-    }, [])
     return (
         <div className=" flex justify-between items-center lg:flex-row flex-col-reverse gap-10 w-full px-36 bg-primary ">
             <div className=" relative flex justify-center items-center lg:items-start flex-col gap-5 mb-28">
@@ -36,9 +22,9 @@ export default function HeaderProductBox({ id, title, collection, images, unit_p
             </div>
             <div className=" z-50 ">
                 <div className=" w-80 h-96  lg:w-[32rem] lg:h-[43rem]  relative after:absolute after:bg-[url('/images/png/next-btn.png')] after:bg-no-repeat after:w-10 after:h-44 after:right-0 after:bottom-5 ">
-                    <img src={images[0]?.image} className='rounded-[2.5rem]' alt="product" />
+                    <img src={images && images[0]?.image} className='rounded-[2.5rem]' alt="product" />
                     <div className=" absolute -left-24 bottom-28 lg:-left-52 lg:bottom-48  -rotate-90">
-                        <span className=' text-white text-4xl lg:text-6xl font-outline-1 drop-shadow-lg uppercase'>{singleCollection?.title}</span>
+                        <span className=' text-white text-4xl lg:text-6xl font-outline-1 drop-shadow-lg uppercase'>Collection</span>
                     </div>
                     <div className=" z-50 absolute -left-14 -top-14" data-swiper-parallax="-400">
                         <SpinnerIcon color='text-primary' />
---
+++ src/Components/Modules/Tag/Tag.tsx
@@ -0,0 +1,14 @@
+
+interface ITag {
+    keyTag: string;
+    valueTag: string | number;
+}
+
+export default function Tag({keyTag, valueTag}: ITag) {
+  return (
+    <div className="">
+    <span className=' font-bold capitalize '>{keyTag}:</span>
+    <span className=' ml-1 text-sm text-zinc-400 capitalize'>{valueTag}</span>
+</div>
+  )
+}
